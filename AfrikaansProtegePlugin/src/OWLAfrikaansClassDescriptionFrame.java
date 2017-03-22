29099d1949dfcb84c54a5e81256e61bf00f6e623
Adam Kaliski <adam.kaliski47@gmail.com>
2017-03-22 22:11:28 +0200

    Added general class axioms frame section and rows.

--- AfrikaansProtegePlugin/src/OWLAfrikaansClassDescriptionFrame.java
+++ AfrikaansProtegePlugin/src/OWLAfrikaansClassDescriptionFrame.java
@@ -1,6 +1,7 @@
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.frame.AbstractOWLFrame;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
 
 /**
  * Created by Casey on 17-Mar-17.
@@ -12,5 +13,7 @@ public class OWLAfrikaansClassDescriptionFrame extends AbstractOWLFrame<OWLClass
         addSection(new OWLAfikaansEquivalentClassesAxiomFrameSection(editorKit, this));
         addSection(new OWLAfrikaansSubClassesAxiomFrameSection(editorKit, this));
         addSection(new OWLAfrikaansDisjointClassesAxiomFrameSection(editorKit, this));
+        addSection(new OWLAfrikaansGeneralClassAxiomFrameSection(editorKit, this));
+
     }
 }
